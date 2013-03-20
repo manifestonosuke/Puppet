c12d32ae232c8f1927173b49e8614830a521859d
manifestonosuke <manifestonosuke@gmail.com>
2013-03-20 15:25:54 +0100

    test

--- users.pp
+++ users.pp
@@ -1,11 +1,20 @@
+
 class virt_groups {
 	@group { "local": 
-	gid => "10000",
-	ensure => present
+	ensure => present,
+	gid => "local",
 	}
 }
 
-class virt_users {
+class virt_user::define {
+	@user { "test": 
+	ensure => "present",
+	uid => "20000",
+	gid => "local",
+	comment => "Test Puppet User",
+	home => "/home/test",
+	shell => "/bin/bash",
+	}
 	@user { 
 	"pierre": ensure => "present",
 	uid => "10705",
@@ -14,22 +23,45 @@ class virt_users {
 	home => "/home/pierre",
 	shell => "/bin/bash",
 	}
-	@user { 
-	"test": ensure => "present",
-	uid => "20000",
-	gid => "local",
-	comment => "Test User",
-	home => "/home/test",
-	shell => "/bin/bash",
-	}
+        @user {
+        "nimda": ensure => "present",
+        uid => "10001",
+        gid => "local",
+        comment => "Admin user",
+        home => "/home/nimda",
+        shell => "/bin/bash",
+        }
+
 }
 
-class custom_users {
-	include virt_users, virt_groups
+class virt_user::admin inherits virt_user::define {
+	include virt_groups, sudo_group
+	User["pierre"] {groups +> "$sudo_group"}
+	User["nimda"] {groups +> "$sudo_group"}
 	realize(
 		Group["local"],
 		User["pierre"],
+		User["nimda"],
+	)
+}
+
+class virt_user::common inherits virt_user::define {
+	include virt_groups
+	realize(
+		Group["local"],
 		User["test"],
 	)
+}
+
+
+class sudo_group {
+  case $operatingsystem {
+    'RedHat', 'CentOS','Fedora','Gentoo': {
+      $sudo_group = "wheel"
+    }
+    'Debian','Ubuntu': {
+      $sudo_group = "sudo"
+    }
+  }
 }
 
